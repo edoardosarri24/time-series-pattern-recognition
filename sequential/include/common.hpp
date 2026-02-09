13d4b7e96fe058d0446f689d61be228cd9ee889d
edoardosarri24 <sarriedoardo24@gmail.com>
2026-02-09 10:12:35 +0100

    auto padding

--- sequential/include/common.hpp
+++ sequential/include/common.hpp
@@ -2,10 +2,11 @@
 #define COMMON_HPP
 
 #include <cstddef>
+#include <bit>
 
 namespace constants {
     constexpr std::size_t ORIGINAL_DIM = 6;
-    constexpr std::size_t PADDED_DIM = 8; // Aligned to 32 bytes (8 * 4 bytes)
+    constexpr std::size_t PADDED_DIM = std::bit_ceil(ORIGINAL_DIM); // Aligned to next power of 2.
     constexpr std::size_t QUERY_LENGTH = 64; // 1/2 of the whole human movement.
 }
 
